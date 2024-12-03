b990ea64ebb537e6cec50558e4f2cc1745da5ee8
Stepan Antonyuk <stepan.antonyuk@gmail.com>
2024-12-02 19:32:32 -0600

    feat: obj reader for teasting reanderer and some generick objects

---
+++ src/objLoader.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def read_obj(fileName):
+    vertices = []
+    triangles = []
+    f = open(fileName)
+    for line in f:
+        if line[:2] == "v ":
+            index1 = line.find(" ") + 1
+            index2 = line.find(" ", index1 + 1)
+            index3 = line.find(" ", index2 + 1)
+            
+            vertex = [float(line[index1:index2]), float(line[index2:index3]), float(line[index3:-1]), 1]
+            vertices.append(vertex)
+
+        elif line[0] == "f":
+            index1 = line.find(" ") + 1
+            index2 = line.find(" ", index1 + 1)
+            index3 = line.find(" ", index2 + 1)
+
+            triangles.append([int(line[index1:index2]) - 1, int(line[index2:index3]) - 1, int(line[index3:-1]) - 1])
+
+    f.close()
+
+    return np.asarray(vertices), np.asarray(triangles)
+
